62b74af1bcdf281b0a53a80eabb43ee05eecbf5d
Anthony <you@example.com>
2026-04-22 14:52:28 +0200

    changed logo icon and button color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,17 +6,7 @@ export default function Navbar({ onJoinUs }) {
     <nav className="navbar">
       <div className="navbar__logo">
         <div className="navbar__logo-icon">
-          <svg width="18" height="18" viewBox="0 0 18 18" fill="none">
-            <rect width="18" height="18" rx="4" fill="#4F46E5" />
-            <polyline
-              points="4,9 7.5,12.5 14,6"
-              stroke="white"
-              strokeWidth="2.2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              fill="none"
-            />
-          </svg>
+          <img src="/Logo-Icon.png" alt="dashboard"/>
         </div>
         <span className="navbar__logo-text">Dozy</span>
       </div>
